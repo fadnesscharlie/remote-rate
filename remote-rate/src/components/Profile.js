f001a392618639ebd8b7147867d7c05f27864df0
Charlie <boy_916@hotmail.com>
2021-08-31 19:41:11 -0700

    finished prep lab

---
+++ remote-rate/src/components/Profile.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Profile.css';
+
+class Profile extends React.Component {
+  render() {
+    return (
+      <h1>Remote Rate</h1>
+
+      // if user is logged in, show information
+      // If user logs out, take them back to the home page?
+      //    Or not show information
+
+      // Show their saved data.
+      //    If no saved data, create a sample?
+
+      // Saved Data includes
+      //    Offer: int,
+      //    MPG: int,
+      //    Remote or not: boolean,
+      //    Commute Distance: int,
+      //    email: string,
+      //    data from past offers: object,
+      
+      // Show most recently selected Data from State
+      // Display thier savings from their selected offer
+
+      // Have a button to go to Details and Comparison page
+
+    )
+  }
+}
+
+export default Profile;
