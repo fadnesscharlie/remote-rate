9bd10b818a7749de2e2294b3f5cdc91b30009337
Zach-Winterton <schenley67@gmail.com>
2021-09-01 20:22:16 -0600

    Auth0

--- remote-rate/src/components/Home.js
+++ remote-rate/src/components/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Home.css';
+import LoginButton from './components/LoginButton';
 
 class Home extends React.Component {
   render() {
     return (
+      <>
       <h1>Remote Rate</h1>
-
+      <LoginButton/>
+      <LogoutButton/>
       // Header goes here
-
+      </>
       // Log in button
       // Once user is logged in, take them to their profile page
 
